fc84b545ea27b5da2279eb57601b7a44edfef809
Gabor Szabo <gabor@szabgab.com>
2020-10-30 19:58:52 +0200

    newline

--- tests/test_temperature.py
+++ tests/test_temperature.py
@@ -1,11 +1,17 @@
 import os
 import subprocess
 import pytest
+import sys
 
 TESTS_CASES = [
     (b"F 100", "37.77777777777778", "100.0", "310.92777777777775"),
 ]
 
+
+# We need this tweak for GitHub Actions, I wonder if we also need this on a Windows machine.
+NL = "\n"
+if sys.platform == 'windows':
+  NL = "\r\n"
 
 @pytest.mark.parametrize("stdn,celsius,fahrenheit,kelvin", TESTS_CASES)
 def test_temperature(stdn, celsius, fahrenheit, kelvin):
@@ -15,6 +21,6 @@ def test_temperature(stdn, celsius, fahrenheit, kelvin):
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     out, err = proc.communicate(input=stdn)
-    x = f'Please enter a temperature unit identifying character and a value:\n{celsius} C\n{fahrenheit} F\n{kelvin} K\n'
+    x = f'Please enter a temperature unit identifying character and a value:{NL}{celsius} C{NL}{fahrenheit} F{NL}{kelvin} K{NL}'
     assert out == x.encode()
     assert err == b''
