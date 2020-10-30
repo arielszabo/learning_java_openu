8b6342884b2c7ca9ce0bdf3ac5ec4ea23a93f62a
arielszabosf <ariel.szabo@superfly.com>
2020-10-30 18:59:08 +0200

    add Temperature.java exercise solution

---
+++ Temperature.java
@@ -0,0 +1,55 @@
+/*
+Converts temperature from one unit (celsius, fahrenheit and kelvin) to all three units.
+
+By: Ariel Szabo
+Date: 2020-10-30
+*/
+import java.util.Scanner;
+
+public class Temperature
+{
+    public static void main (String [] args)
+    {
+        // define constants
+        final double C_TO_K_INTERCEPT = 273.15;  // absolute zero
+        final double F_TO_C_COEFFICIENT = 5 / 9.0;
+        final double C_TO_F_COEFFICIENT = 1 / F_TO_C_COEFFICIENT;
+        final int C_TO_F_INTERCEPT = 32;
+        final int F_TO_C_INTERCEPT = - C_TO_F_INTERCEPT;
+
+
+        // read input temperature value and identifier from user
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Please enter a temperature unit identifying character and a value:");
+
+        char unitChar = scan.next().charAt(0);
+        double inputTemperature = scan.nextDouble(); 
+
+        // define the temperature converting traget variables
+        double celsiusTemperature;
+        double fahrenheitTemperature;
+        double kelvinTemperature;
+
+       switch(unitChar) {
+            case 'F': 
+                fahrenheitTemperature = inputTemperature;
+                celsiusTemperature = F_TO_C_COEFFICIENT * (fahrenheitTemperature + F_TO_C_INTERCEPT);
+                kelvinTemperature = celsiusTemperature + C_TO_K_INTERCEPT;
+                break;
+            case 'K':
+                kelvinTemperature = inputTemperature;
+                celsiusTemperature = kelvinTemperature - C_TO_K_INTERCEPT;
+                fahrenheitTemperature = C_TO_F_COEFFICIENT  * celsiusTemperature + C_TO_F_INTERCEPT;
+                break;
+            default: //case 'C':
+                celsiusTemperature = inputTemperature;
+                kelvinTemperature = celsiusTemperature + C_TO_K_INTERCEPT;
+                fahrenheitTemperature = C_TO_F_COEFFICIENT  * celsiusTemperature + C_TO_F_INTERCEPT;
+                break;
+
+       }
+        System.out.println(celsiusTemperature + " C");
+        System.out.println(fahrenheitTemperature + " F");
+        System.out.println(kelvinTemperature + " K");
+    } // end of method main
+} //end of class Temperature
