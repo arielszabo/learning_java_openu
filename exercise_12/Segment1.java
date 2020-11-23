d18e3dfefbb4298ef14a68dcd94dda8fa71d7e75
arielszabo <arielszabo@gmail.com>
2020-11-23 18:35:21 +0200

    refactor overlap

--- exercise_12/Segment1.java
+++ exercise_12/Segment1.java
@@ -234,31 +234,29 @@ public class Segment1
         double otherLeftX = other.getPoLeft().getX();
         double otherRightX = other.getPoRight().getX();
 
-
-        if (thisLeftX >= otherLeftX && thisRightX <= otherLeftX) {
+        if (otherLeftX <= thisLeftX && thisRightX <= otherRightX) {
             // this:     l____r
             // other: l__________r
             return thisRightX - thisLeftX;
+
+        else if (otherLeftX <= thisLeftX && thisRightX > otherRightX) {
+            // this:     l_____r
+            // other: l_____r
+            return otherRightX - thisLeftX;
 
         } else if (thisLeftX <= otherLeftX && otherRightX <= thisRightX) {
             // this: l__________r
             // other:   l____r
             return otherRightX - otherLeftX;
 
-        } else if ( thisRightX >= otherLeftX ) {
+        } else if ( thisLeftX <= otherLeftX && otherRightX > thisRightX ) {
             // this: l_____r
             // other:   l____r
             return thisRightX - otherLeftX;
 
-        } else if ( thisLeftX >= otherLeftX) {
-            // this:     l_____r
-            // other: l_____r
-            return otherRightX - thisLeftX;
-
         } else {
             //  l_____r  l_____r
             return 0.0;
-        }
     }
 
     /**
